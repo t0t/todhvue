95eaabcd7757572a42583df9d061850c14471011
Sergio Fores <sergiofores@gmail.com>
2020-04-26 18:50:41 +0200

    json data dynamic import

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="home">
+    <!-- <div 
+    v-for="cita in citas" v-bind:key="cita" 
+    >
+    <p>{{cita}} lorem </p>
+    </div> -->
+
     <Cover
-      title="sergio fores"
-      subtitle="Original 3D-printed Artwork & Modern prototyping for the web"
+      v-bind:title="data.author"
+      v-bind:subtitle="data.slogans[0].web"
       bgimg="img4.jpg"
     />
     <Footer />
@@ -13,18 +19,31 @@
 <script>
 import Cover from "@/components/organos/Cover.vue";
 import Footer from "@/components/molecules/Footer.vue";
+import todh_data from '@/data';
+console.log(todh_data)
 
 export default {
   name: "Home",
   components: {
     Cover,
     Footer
-  }
+  },
+  data: () => {
+    return {
+      data: todh_data
+    }
+  },
+  // created () {
+  //   this.fetchAllCategories()
+  //     .then(categories => Promise.all(categories.map(category => this.fetchForums({ids: Object.keys(category.forums)}))))
+  //     .then(() => {
+  //       this.ready = true
+  //     })
+  // }
 };
 </script>
 
 <style lang="scss">
-@import "@/styles/main.scss";
 
 .home .Cover {
   height: 100vh;
