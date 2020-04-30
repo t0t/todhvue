a606d6bb197e2f4c32d1d71c5b05799994e235d0
Sergio Fores <sergiofores@gmail.com>
2020-04-30 14:11:59 +0200

    ajustes main nav

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
-    <div class="BrandToggle" role="logo" v-on:click="show = !show">
-      <IcoLogo />
-      <div class="closemenu" v-if="show">Close X</div>
+    <div class="togglemenu" 
+    v-on:click="show = !show; isActive = true"
+    v-bind:class="{ active : isActive }"
+    >
+      <div class="open-menu" v-if="!show">|||</div>
+      <div class="close-menu" v-if="show">X</div>
     </div>
-    <nav id="MainNav" role="navigation">
+    <BrandLogo />
+    <nav id="nav-container" role="navigation">
       <transition name="fade">
-        <ul v-if="show">
+        <ul v-if="show" v-on:click="show = !show; isActive = false">
           <router-link to="/artwork">Artwork</router-link>
           <router-link to="/design">Prototyping</router-link>
           <router-link to="/info">TODH</router-link>
@@ -18,16 +22,17 @@
 </template>
 
 <script>
-import IcoLogo from "@/components/atoms/icons/IcoLogo.vue";
+import BrandLogo from "@/components/molecules/BrandLogo.vue";
 
 export default {
   data: () => {
     return {
       show: false,
+      isActive: false
     };
   },
   components: {
-    IcoLogo,
+    BrandLogo,
   },
 };
 </script>
@@ -42,90 +47,69 @@ export default {
   text-align: center;
 }
 
-.BrandToggle {
+.togglemenu {
+  width: 60px;
+  height: 60px;
   position: fixed;
-  top: $h2;
-  right: 40%;
-  left: 40%;
-  z-index: 2;
-  &:hover {
-    cursor: pointer;
-  }
-  & path.sentir {
-    fill: $white;
-    stroke: none;
-  }
-  & path.hacer {
-    stroke: $dark_grey;
-    stroke-width: 0.5px;
-  }
-  & circle {
-    fill: $secondary;
-  }
-  svg {
-    width: $h4;
-    height: $h4;
-  }
-  .closemenu {
-    color: $white;
-  }
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: space-between;
+  align-items: center;
+  top: 0;
+  right: 0;
+  z-index: 9;
   @include media(s4) {
     width: $h5;
     top: 0;
     height: $h4;
     right: 0;
     left: auto;
-    padding-top: $h1;
+  }
+  &:hover {
+    cursor: pointer;
+  }
+  .close-menu {
+    color: $white;
+  }
+  .close-menu, .open-menu {
+    padding: $h1;
   }
 }
-#MainNav {
+
+#nav-container {
   ul {
     position: fixed;
     top: 0;
-    height: $h6;
+    @include padding-top(3);
+    height: 100vh;
     width: 100%;
     right: 0;
-    padding: 0;
     display: flex;
-    justify-content: center;
-    align-items: flex-end;
-    z-index: 1;
+    justify-content: flex-start;
+    flex-direction: column;
+    align-items: center;
     background-color: $alpha_black;
     @include media(s1) {
       transform: translateY(0);
-    }
-    @include media(s2) {
-      right: 0;
-      padding: 0;
-      height: $h6;
-    }
-    @include media(s4) {
-      height: $h4;
     }
   }
   a {
     color: $white;
     padding: $h0;
-    @include media(s1) {
-      padding: $h0;
-      background-color: $secondary;
-      font-size: $h1;
+    font-size: $h2;
+    &:hover {
+    background-color: $alpha_black;
     }
+    @include margin-top(1);
     @include media(s2) {
-      display: block;
       padding: $h1;
-      background-color: transparent;
-      font-size: $h1;
-      &:hover {
-        background-color: $secondary;
-      }
     }
     @include media(s4) {
       font-size: $h2;
     }
     &.router-link-exact-active {
       color: $tertiary;
-      border-bottom: $h-1 solid $black_light;
+      border: 1px solid $white;
 
       &:hover {
         cursor: pointer;
@@ -133,12 +117,11 @@ export default {
     }
   }
   .fade-enter-active {
-     animation: menu .1s 1;
+    animation: menu 0.1s 1;
   }
   .fade-leave-active {
-     animation: menu .1s 1;
-     animation-direction: alternate-reverse;
+    animation: menu 0.1s 1;
+    animation-direction: alternate-reverse;
   }
-  
 }
 </style>
---
+++ src/components/molecules/BrandLogo.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="BrandLogo" role="logo">
+    <router-link to="/">
+      <IcoLogo />
+    </router-link>
+  </div>
+</template>
+
+<script>
+import IcoLogo from "@/components/atoms/icons/IcoLogo.vue";
+export default {
+  name: "BrandLogo",
+  components: {
+    IcoLogo,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "@/styles/main.scss";
+
+.togglemenu.active ~ .BrandLogo {
+  & path {
+    fill: $white;
+  }
+  & circle {
+    fill: $white;
+  }
+  @include media(s0) {
+    left: 36%;
+  }
+  @include media(s1) {
+    left: 43.8%;
+  }
+  @include media(s2) {
+    left: 46%;
+  }
+  @include media(s2) {
+    left: 50%;
+  }
+}
+.BrandLogo {
+  z-index: 1000;
+  height: 50px;
+  width: 50px;
+  padding: 10px;
+  position: fixed;
+  &:hover {
+    & path.hacer {
+      fill: $black;
+    }
+    & path.sentir {
+      fill: blue;
+      stroke: none;
+    }
+    & path {
+      fill: yellow;
+    }
+    & circle {
+      fill: red;
+    }
+  }
+}
+</style>
